6b5962190856e10732abadd015010712b57d844e
Dante Luis <dsldante@gmail.com>
2024-10-01 17:24:01 -0400

    Research on applying machine learning techniques to data sets

---
+++ mne_test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import mne
+
+sample_data_folder = mne.datasets.sample.data_path()
+sample_data_raw_file = (
+    sample_data_folder / "MEG" / "sample" / "sample_audvis_filt-0-40_raw.fif"
+)
+
+raw = mne.io.read_raw_fif(sample_data_raw_file)
+
+print(raw)
+print(raw.info)
+
+raw.compute_psd(fmax=50).plot(picks="data", exclude="bads", amplitude=False)
+raw.plot(duration=5, n_channels=30)
+input()
