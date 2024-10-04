a3a3e6734036c7b60d110d0fb6ba5493f0efa070
Dante Luis <dsldante@gmail.com>
2024-10-04 14:18:02 -0400

    Progress update - working on mne and how it may connect to EEG data for machine learning

--- EEG_Processing/mne_test.py
+++ EEG_Processing/mne_test.py
@@ -1,5 +1,5 @@
-import numpy as np
 import mne
+import numpy as np
 
 sample_data_folder = mne.datasets.sample.data_path()
 sample_data_raw_file = (
@@ -8,10 +8,15 @@ sample_data_raw_file = (
 
 raw = mne.io.read_raw_fif(sample_data_raw_file)
 
-print(raw)
-print(raw.info)
-
-raw.compute_psd(fmax=50).plot(picks="data", exclude="bads", amplitude=False)
-raw.plot(duration=5, n_channels=30)
+# print(raw)
+# print(raw.info)
+#
+# raw.compute_psd(fmax=50).plot(picks="data", exclude="bads", amplitude=False)
+# raw.plot(duration=5, n_channels=30)
 
 ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
+ica.fit(raw)
+ica.exclude = [1, 2]
+ica.plot_properties(raw, picks=ica.exclude)
+
+input("Press Enter to continue...")
