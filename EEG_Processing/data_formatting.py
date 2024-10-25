e01c03426801526e8c44b3210cb98f354b1aca7c
Dante Luis <dsldante@gmail.com>
2024-10-25 17:17:28 -0400

    Somatosensory dataset exploration

--- EEG_Processing/data_formatting.py
+++ EEG_Processing/data_formatting.py
@@ -1,3 +1,14 @@
+"""
+Somatosensory data is relating to the sense of touch and perception of the body and its movements.
+
+Dataset provides somatosensory data with event related synchronizations and desynchronizations.
+
+Goal:       Gain a deeper understanding of the somatosensory data along with mne and its components.
+Author:     Dante Luis
+Sources:    https://mne.tools/stable/documentation/datasets.html#somato-dataset
+            https://mne.tools/stable/auto_tutorials/time-freq/20_sensors_time_frequency.html#tut-sensors-time-freq
+"""
+
 import mne
 import numpy as np
 
@@ -12,7 +23,7 @@ raw_data.crop(120, 360).load_data().resample(200)
 
 events = mne.find_events(raw_data, stim_channel="STI 014")
 
-picks = mne.pick_types(raw_data.info, meg="grad", eeg=False, eog=True, stim=False)
+picks = mne.pick_types(raw_data.info, meg="grad", eeg=False, eog=True, stim=False)  # type: ignore
 
 event_id, tmin, tmax = 1, -1.0, 3.0
 baseline = (None, 0)
@@ -28,3 +39,11 @@ epochs = mne.Epochs(
     reject=dict(grad=4000e-13, eog=350e-6),
     preload=True,
 )
+
+epochs.compute_psd(fmin=2, fmax=40).plot(
+    average=True, amplitude=False, picks="data", exclude="bads"
+)
+input()
+
+epochs.compute_psd().plot_topomap(ch_type="grad", normalize=False, contours=0)
+input()
