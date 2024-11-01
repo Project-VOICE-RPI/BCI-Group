e6ffa46b41fb61d4e38e344cbdf87b287b07021f
bjk2023 <barbarakotlannico@gmail.com>
2024-11-01 16:44:56 -0400

    Starting module code/set up

--- BCI2000/src/shared/modules/signalprocessing/TestModSignalProcessing.cpp
+++ BCI2000/src/shared/modules/signalprocessing/TestModSignalProcessing.cpp
@@ -1,1 +1,19 @@
+#include "TestModSignalProcessing.h"
+#include <H5Cpp.h>
+#include <vector>
 
+int main() {
+    H5::H5File file("path_to_file.snirf", H5F_ACC_RDONLY);
+    H5::DataSet dataset = file.openDataSet("/nirs/data1/dataTimeSeries");
+
+    // Retrieve dataset dimensions, create a buffer, and read data
+    H5::DataSpace dataspace = dataset.getSpace();
+    hsize_t dims[2];
+    dataspace.getSimpleExtentDims(dims, NULL);
+    std::vector<double> data(dims[0] * dims[1]);
+    dataset.read(data.data(), H5::PredType::NATIVE_DOUBLE);
+
+    // Save or process data here
+    file.close();
+    return 0;
+}
