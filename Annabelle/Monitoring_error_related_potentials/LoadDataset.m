a8b1d3283a03367c42a7c705a497993efa7e313c
AnnabelleYChan <chana8@rpi.edu>
2024-11-19 20:05:59 -0500

    Added the event information to eeglab

--- Annabelle/Monitoring_error_related_potentials/LoadDataset.m
+++ Annabelle/Monitoring_error_related_potentials/LoadDataset.m
@@ -1,19 +1,27 @@
 % Makes Variables EEGij_k with i = subject number, j = session number, and
 % k = run number and assigns the raw eeg data in EEGLAB format
 
-for i = 1:6 % subject 
+for i = 1:1 % subject 
     for j = 1:2 % session 
         File = ['Subject0', num2str(i), '_s', num2str(j), '.mat'];
-        [EEGs, ~, ~, ~, ~] = Parser(File);
+        [EEGs, Positions, Types, ~, Labels] = Parser(File);
 
         for k = 1:10 % run
-            varName = sprintf('EEG%d%d_%d', i, j, k);
+            eegName = sprintf('EEG%d%d_%d', i, j, k);
+            eventName = sprintf('Event%d%d_%d', i, j, k);
             
             %convert for samples x channels to channels x samples
             eeg = transpose(EEGs{k});
+            event = eventArray(Positions{k}, Types{k});
 
-            eval([varName ' = eeg;']);
+            eval([eegName ' = eeg;']);
+            eval([eventName ' = event;']);
         end
 
     end
+end
+
+function event = eventArray(Positions, Types)
+    m = size(Positions, 1);
+    event = [Positions Types];
 end
---
+++ Annabelle/eeglab2024.2/theVoice/loadEEGScript.m
@@ -0,0 +1,20 @@
+[ALLEEG, EEG, CURRENTSET, ALLCOM] = eeglab;
+
+for i = 1:1 % subject 
+    for j = 1:2 % session 
+        for k = 1:10 % run
+            eegName = sprintf('EEG%d%d_%d', i, j, k);
+            eventName = sprintf('Event%d%d_%d', i, j, k);
+            dataName = sprintf('Subject_%d%d_s%d', i, j, k);
+
+            EEG = pop_importdata('dataformat','array','nbchan',0,'data', eegName,'srate',512,'pnts',0,'xmin',0);
+            [ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname',dataName,'gui','off'); 
+
+            EEG = pop_importevent( EEG, 'event',eval(eventName),'fields',{'latency','type'},'timeunit',NaN);
+            [ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET);
+
+            eeglab redraw;
+        end
+
+    end
+end
