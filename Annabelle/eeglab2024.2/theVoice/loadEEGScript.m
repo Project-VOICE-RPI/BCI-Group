13c66a2f104175d4f1d03b34521ff3d54320be99
AnnabelleYChan <chana8@rpi.edu>
2024-11-22 17:57:20 -0500

    Cleaned the data

--- Annabelle/eeglab2024.2/theVoice/loadEEGScript.m
+++ Annabelle/eeglab2024.2/theVoice/loadEEGScript.m
@@ -1,6 +1,6 @@
 [ALLEEG, EEG, CURRENTSET, ALLCOM] = eeglab;
 
-for i = 1:6 % subject 
+for i = 1:1 % subject 
     for j = 1:2 % session 
         for k = 1:10 % run
             eegName = sprintf('EEG%d%d_%d', i, j, k);
@@ -11,6 +11,13 @@ for i = 1:6 % subject
             [ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname',dataName,'gui','off'); 
 
             EEG = pop_importevent( EEG, 'event',eval(eventName),'fields',{'latency','type'},'timeunit',NaN);
+            
+            % auto clean
+            EEG = pop_clean_rawdata(EEG, 'FlatlineCriterion',5,'ChannelCriterion',0.8,'LineNoiseCriterion',4,'Highpass','off','BurstCriterion',20,'WindowCriterion',0.25,'BurstRejection','on','Distance','Euclidian','WindowCriterionTolerances',[-Inf 7] );
+            
+            % notch filter
+            EEG = pop_eegfiltnew(EEG, 'hicutoff',60,'revfilt',1,'plotfreqz',0);
+            
             [ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET);
 
             eeglab redraw;
